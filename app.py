b3bb7801de0cd93f074bd8f73d386fb66a476e5e
chinthaka <chinthaka.wit@gmail.com>
2025-01-26 17:37:05 +0530

    18

--- app.py
+++ app.py
@@ -21,12 +21,14 @@ db_config = {
     # "port": 3307
 }
 
+
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if "user_id" not in session:
             return redirect(url_for("login"))
         return f(*args, **kwargs)
+
     return decorated_function
 
 
