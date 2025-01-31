67b45fca0bdab29d5a4e2e70014bd41a6bc31b1f
chinthaka <chinthaka.wit@gmail.com>
2025-01-31 11:47:53 +0530

    26

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for, session, url_for
+from flask import Flask, request, jsonify, render_template, redirect, url_for, session
 import mysql.connector
 import bcrypt
 import json
